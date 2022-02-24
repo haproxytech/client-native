d6c67af2a3b71afe590062eebecb0fd8230e5ac5
Goran Galinec <ggalinec@haproxy.com>
2022-02-24 09:09:29 +0100

    BUG/MEDIUM: maps: return an empty list instead of nil for an empty maps file

--- runtime/runtime_client.go
+++ runtime/runtime_client.go
@@ -670,10 +670,10 @@ func (c *Client) ShowMapEntries(name string) (models.MapEntries, error) {
 			}
 		}
 	}
-	if len(entries) > 0 {
-		return entries, nil
+	if lastErr != nil {
+		return nil, lastErr
 	}
-	return nil, lastErr
+	return entries, nil
 }
 
 // ShowMapEntriesVersioned list all map entries by map file name
@@ -708,10 +708,10 @@ func (c *Client) ShowMapEntriesVersioned(name, version string) (models.MapEntrie
 			}
 		}
 	}
-	if len(entries) > 0 {
-		return entries, nil
+	if lastErr != nil {
+		return nil, lastErr
 	}
-	return nil, lastErr
+	return entries, nil
 }
 
 // AddMapPayload adds multiple entries to the map file
