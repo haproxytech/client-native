9c7f34e6d7d0c61dc947521e6ef1c192b6390ca7
George Vine <gvine@haproxy.com>
2022-11-16 22:58:30 -0500

    BUG/MEDIUM: spec: add argument support for option originalto

--- models/backend.go
+++ models/backend.go
@@ -188,8 +188,7 @@ type Backend struct {
 	Nolinger string `json:"nolinger,omitempty"`
 
 	// originalto
-	// Enum: [enabled disabled]
-	Originalto string `json:"originalto,omitempty"`
+	Originalto *Originalto `json:"originalto,omitempty"`
 
 	// persist
 	// Enum: [enabled disabled]
@@ -1643,43 +1642,20 @@ func (m *Backend) validateNolinger(formats strfmt.Registry) error {
 	return nil
 }
 
-var backendTypeOriginaltoPropEnum []interface{}
-
-func init() {
-	var res []string
-	if err := json.Unmarshal([]byte(`["enabled","disabled"]`), &res); err != nil {
-		panic(err)
-	}
-	for _, v := range res {
-		backendTypeOriginaltoPropEnum = append(backendTypeOriginaltoPropEnum, v)
-	}
-}
-
-const (
-
-	// BackendOriginaltoEnabled captures enum value "enabled"
-	BackendOriginaltoEnabled string = "enabled"
-
-	// BackendOriginaltoDisabled captures enum value "disabled"
-	BackendOriginaltoDisabled string = "disabled"
-)
-
-// prop value enum
-func (m *Backend) validateOriginaltoEnum(path, location string, value string) error {
-	if err := validate.EnumCase(path, location, value, backendTypeOriginaltoPropEnum, true); err != nil {
-		return err
-	}
-	return nil
-}
-
 func (m *Backend) validateOriginalto(formats strfmt.Registry) error {
 	if swag.IsZero(m.Originalto) { // not required
 		return nil
 	}
 
-	// value enum
-	if err := m.validateOriginaltoEnum("originalto", "body", m.Originalto); err != nil {
-		return err
+	if m.Originalto != nil {
+		if err := m.Originalto.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("originalto")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("originalto")
+			}
+			return err
+		}
 	}
 
 	return nil
@@ -2240,6 +2216,10 @@ func (m *Backend) ContextValidate(ctx context.Context, formats strfmt.Registry) 
 		res = append(res, err)
 	}
 
+	if err := m.contextValidateOriginalto(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.contextValidatePgsqlCheckParams(ctx, formats); err != nil {
 		res = append(res, err)
 	}
@@ -2402,6 +2382,22 @@ func (m *Backend) contextValidateMysqlCheckParams(ctx context.Context, formats s
 				return ve.ValidateName("mysql_check_params")
 			} else if ce, ok := err.(*errors.CompositeError); ok {
 				return ce.ValidateName("mysql_check_params")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Backend) contextValidateOriginalto(ctx context.Context, formats strfmt.Registry) error {
+
+	if m.Originalto != nil {
+		if err := m.Originalto.ContextValidate(ctx, formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("originalto")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("originalto")
 			}
 			return err
 		}
--- models/defaults.go
+++ models/defaults.go
@@ -262,8 +262,7 @@ type Defaults struct {
 	Nolinger string `json:"nolinger,omitempty"`
 
 	// originalto
-	// Enum: [enabled disabled]
-	Originalto string `json:"originalto,omitempty"`
+	Originalto *Originalto `json:"originalto,omitempty"`
 
 	// persist
 	// Enum: [enabled disabled]
@@ -2213,43 +2212,20 @@ func (m *Defaults) validateNolinger(formats strfmt.Registry) error {
 	return nil
 }
 
-var defaultsTypeOriginaltoPropEnum []interface{}
-
-func init() {
-	var res []string
-	if err := json.Unmarshal([]byte(`["enabled","disabled"]`), &res); err != nil {
-		panic(err)
-	}
-	for _, v := range res {
-		defaultsTypeOriginaltoPropEnum = append(defaultsTypeOriginaltoPropEnum, v)
-	}
-}
-
-const (
-
-	// DefaultsOriginaltoEnabled captures enum value "enabled"
-	DefaultsOriginaltoEnabled string = "enabled"
-
-	// DefaultsOriginaltoDisabled captures enum value "disabled"
-	DefaultsOriginaltoDisabled string = "disabled"
-)
-
-// prop value enum
-func (m *Defaults) validateOriginaltoEnum(path, location string, value string) error {
-	if err := validate.EnumCase(path, location, value, defaultsTypeOriginaltoPropEnum, true); err != nil {
-		return err
-	}
-	return nil
-}
-
 func (m *Defaults) validateOriginalto(formats strfmt.Registry) error {
 	if swag.IsZero(m.Originalto) { // not required
 		return nil
 	}
 
-	// value enum
-	if err := m.validateOriginaltoEnum("originalto", "body", m.Originalto); err != nil {
-		return err
+	if m.Originalto != nil {
+		if err := m.Originalto.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("originalto")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("originalto")
+			}
+			return err
+		}
 	}
 
 	return nil
@@ -2837,6 +2813,10 @@ func (m *Defaults) ContextValidate(ctx context.Context, formats strfmt.Registry)
 		res = append(res, err)
 	}
 
+	if err := m.contextValidateOriginalto(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.contextValidatePgsqlCheckParams(ctx, formats); err != nil {
 		res = append(res, err)
 	}
@@ -3013,6 +2993,22 @@ func (m *Defaults) contextValidateMysqlCheckParams(ctx context.Context, formats 
 				return ve.ValidateName("mysql_check_params")
 			} else if ce, ok := err.(*errors.CompositeError); ok {
 				return ce.ValidateName("mysql_check_params")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *Defaults) contextValidateOriginalto(ctx context.Context, formats strfmt.Registry) error {
+
+	if m.Originalto != nil {
+		if err := m.Originalto.ContextValidate(ctx, formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("originalto")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("originalto")
 			}
 			return err
 		}
