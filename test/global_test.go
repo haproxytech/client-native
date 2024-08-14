477ae2c452a4fa9788dbdc192e98f32e37a4a4a4
Marko Juraga <mjuraga@haproxy.com>
2024-08-14 10:49:30 +0200

    TEST/MEDIUM: global: update global tests for new changes

--- test/global_test.go
+++ test/global_test.go
@@ -17,7 +17,6 @@ package test
 
 import (
 	_ "embed"
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -47,7 +46,7 @@ func TestGetGlobal(t *testing.T) {
 
 func checkGlobal(t *testing.T, global *models.Global) {
 	want := globalExpcectation()
-	require.True(t, global.GlobalBase.Equal(want.GlobalBase), "diff %v", cmp.Diff(*global, *want))
+	require.True(t, global.GlobalBase.Equal(want.GlobalBase), "diff %v", cmp.Diff(global.GlobalBase, want.GlobalBase))
 }
 
 func TestPutGlobal(t *testing.T) {
@@ -56,44 +55,44 @@ func TestPutGlobal(t *testing.T) {
 	}
 
 	err := clientTest.PushGlobalConfiguration(g, "", version)
-
-	if err != nil {
-		t.Error(err.Error())
-	} else {
-		version++
-	}
+	require.NoError(t, err)
+	version++
 
 	ver, global, err := clientTest.GetGlobalConfiguration("")
-	if err != nil {
-		t.Error(err.Error())
-	}
+	require.NoError(t, err)
 
-	var givenJSON []byte
-	givenJSON, err = g.MarshalBinary()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	var onDiskJSON []byte
-	onDiskJSON, err = global.MarshalBinary()
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	if string(givenJSON) != string(onDiskJSON) {
-		fmt.Printf("Created global: %v\n", string(onDiskJSON))
-		fmt.Printf("Given global: %v\n", string(givenJSON))
-		t.Error("Created global not equal to given global")
-	}
-
+	require.True(t, g.GlobalBase.Equal(global.GlobalBase, models.Options{NilSameAsEmpty: false}), "diff %v", cmp.Diff(g.GlobalBase, global.GlobalBase))
 	if ver != version {
-		t.Error("Version not incremented!")
+		t.Fatal("Version not incremented!")
+	}
+}
+
+func TestPutEmptyGlobal(t *testing.T) {
+	g := &models.Global{}
+
+	err := clientTest.PushGlobalConfiguration(g, "", version)
+	require.NoError(t, err)
+	version++
+
+	ver, global, err := clientTest.GetGlobalConfiguration("")
+	require.NoError(t, err)
+
+	require.True(t, g.GlobalBase.Equal(global.GlobalBase, models.Options{NilSameAsEmpty: false}), "diff %v", cmp.Diff(g.GlobalBase, global.GlobalBase))
+	if ver != version {
+		t.Fatal("Version not incremented!")
 	}
 
-	err = clientTest.PushGlobalConfiguration(g, "", 55)
+	g = globalExpcectation()
+	err = clientTest.PushGlobalConfiguration(g, "", version)
+	require.NoError(t, err)
+	version++
 
-	if err == nil {
-		t.Error("Should have returned version conflict.")
+	ver, global, err = clientTest.GetGlobalConfiguration("")
+	require.NoError(t, err)
+
+	require.True(t, g.GlobalBase.Equal(global.GlobalBase, models.Options{NilSameAsEmpty: false}), "diff %v", cmp.Diff(g.GlobalBase, global.GlobalBase))
+	if ver != version {
+		t.Fatal("Version not incremented!")
 	}
 }
 
@@ -106,7 +105,7 @@ func getGlobalBase() models.GlobalBase {
 	luaPrependPath := "/usr/share/haproxy-lua/?/init.lua"
 	enabled := "enabled"
 	return models.GlobalBase{
-		Daemon: "enabled",
+		Daemon: true,
 		CPUMaps: []*models.CPUMap{
 			{
 				Process: &n,
@@ -121,85 +120,99 @@ func getGlobalBase() models.GlobalBase {
 				},
 			},
 		},
-		Maxconn:                1000,
-		SslDefaultBindCiphers:  "test",
-		SslDefaultBindOptions:  "ssl-min-ver TLSv1.0 no-tls-tickets",
-		SslDefaultServerCurves: "secp384r1",
-		StatsTimeout:           &tOut,
-		ExternalCheck:          false,
-		LuaPrependPath: []*models.LuaPrependPath{
-			{
-				Path: &luaPrependPath,
-				Type: "cpath",
-			},
+		PerformanceOptions: &models.PerformanceOptions{
+			Maxconn:         1000,
+			ProfilingMemory: "enabled",
+			ThreadHardLimit: misc.Int64P(100),
 		},
-		LuaLoads: []*models.LuaLoad{
-			{
-				File: &f,
+		SslOptions: &models.SslOptions{
+			DefaultBindCiphers:         "test",
+			DefaultBindOptions:         "ssl-min-ver TLSv1.0 no-tls-tickets",
+			DefaultServerCurves:        "secp384r1",
+			DefaultServerSigalgs:       "ECDSA+SHA256",
+			DefaultServerClientSigalgs: "ECDSA+SHA256",
+			Propquery:                  "foo",
+			Provider:                   "my_provider",
+			ProviderPath:               "providers/",
+			SecurityLevel:              misc.Int64P(2),
+		},
+		StatsTimeout:  &tOut,
+		ExternalCheck: false,
+		LuaOptions: &models.LuaOptions{
+			PrependPath: []*models.LuaPrependPath{
+				{
+					Path: &luaPrependPath,
+					Type: "cpath",
+				},
+			},
+			Loads: []*models.LuaLoad{
+				{
+					File: &f,
+				},
 			},
 		},
 		LogSendHostname: &models.GlobalLogSendHostname{
 			Enabled: &enabled,
 			Param:   "something",
 		},
-		TuneOptions: &models.GlobalTuneOptions{
-			DisableZeroCopyForwarding:      true,
-			EventsMaxEventsAtOnce:          50,
-			H1ZeroCopyFwdRecv:              "disabled",
-			H1ZeroCopyFwdSend:              "disabled",
-			H2ZeroCopyFwdSend:              "disabled",
-			LuaLogLoggers:                  "disabled",
-			LuaLogStderr:                   "disabled",
-			MaxChecksPerThread:             misc.Int64P(20),
-			PeersMaxUpdatesAtOnce:          100,
-			PtZeroCopyForwarding:           "disabled",
-			QuicFrontendConnTxBuffersLimit: nil,
-			QuicFrontendMaxIdleTimeout:     misc.Int64P(5000),
-			QuicSocketOwner:                "listener",
-			RcvbufBackend:                  misc.Int64P(8192),
-			RcvbufFrontend:                 misc.Int64P(4096),
-			SndbufBackend:                  misc.Int64P(1234),
-			SndbufFrontend:                 misc.Int64P(5678),
-			SslOcspUpdateMaxDelay:          misc.Int64P(48),
-			SslOcspUpdateMinDelay:          misc.Int64P(49),
-			StickCounters:                  misc.Int64P(50),
+		// TuneLuaOptions: &models.TuneLuaOptions{
+		// 	LogLoggers: "disabled",
+		// 	LogStderr:  "disabled",
+		// },
+		TuneBufferOptions: &models.TuneBufferOptions{
+			RcvbufBackend:  misc.Int64P(8192),
+			RcvbufFrontend: misc.Int64P(4096),
+			SndbufBackend:  misc.Int64P(1234),
+			SndbufFrontend: misc.Int64P(5678),
 		},
-		HttpclientResolversDisabled: "disabled",
-		HttpclientResolversPrefer:   "ipv6",
-		HttpclientResolversID:       "my2",
-		HttpclientRetries:           5,
-		HttpclientSslCaFile:         "my_ca_file.ca",
-		HttpclientTimeoutConnect:    misc.Int64P(5000),
-		HttpclientSslVerify:         misc.StringP(""),
-		UID:                         1234,
-		WurflOptions:                &models.GlobalWurflOptions{},
-		DeviceAtlasOptions:          &models.GlobalDeviceAtlasOptions{},
-		FiftyOneDegreesOptions:      &models.GlobalFiftyOneDegreesOptions{},
-		StatsMaxconn:                misc.Int64P(30),
-		Anonkey:                     misc.Int64P(40),
-		NumaCPUMapping:              "disabled",
+		TuneQuicOptions: &models.TuneQuicOptions{
+			FrontendConnTxBuffersLimit: nil,
+			FrontendMaxIdleTimeout:     misc.Int64P(5000),
+			SocketOwner:                "listener",
+		},
+		TuneSslOptions: &models.TuneSslOptions{
+			OcspUpdateMaxDelay: misc.Int64P(48),
+			OcspUpdateMinDelay: misc.Int64P(49),
+		},
+		TuneOptions: &models.TuneOptions{
+			DisableZeroCopyForwarding: true,
+			EventsMaxEventsAtOnce:     50,
+			H1ZeroCopyFwdRecv:         "disabled",
+			H1ZeroCopyFwdSend:         "disabled",
+			H2ZeroCopyFwdSend:         "disabled",
+			MaxChecksPerThread:        misc.Int64P(20),
+			PeersMaxUpdatesAtOnce:     100,
+			PtZeroCopyForwarding:      "disabled",
+			StickCounters:             misc.Int64P(50),
+		},
+		HTTPClientOptions: &models.HTTPClientOptions{
+			ResolversDisabled: "disabled",
+			ResolversPrefer:   "ipv6",
+			ResolversID:       "my2",
+			Retries:           5,
+			SslCaFile:         "my_ca_file.ca",
+			TimeoutConnect:    misc.Int64P(5000),
+			SslVerify:         misc.StringP(""),
+		},
+		DebugOptions: &models.DebugOptions{
+			Anonkey: misc.Int64P(40),
+		},
+		UID:            1234,
+		StatsMaxconn:   misc.Int64P(30),
+		NumaCPUMapping: "disabled",
 		DefaultPath: &models.GlobalDefaultPath{
 			Type: "origin",
 			Path: "/some/other/path",
 		},
-		NoQuic:                        false,
-		ClusterSecret:                 "",
-		SslDefaultServerSigalgs:       "ECDSA+SHA256",
-		SslDefaultServerClientSigalgs: "ECDSA+SHA256",
-		SslPropquery:                  "foo",
-		SslProvider:                   "my_provider",
-		SslProviderPath:               "providers/",
-		Setcap:                        "none",
-		LimitedQuic:                   true,
-		ProfilingMemory:               "enabled",
+		NoQuic:        false,
+		ClusterSecret: "",
+		Setcap:        "none",
+		LimitedQuic:   true,
 		Harden: &models.GlobalHarden{
 			RejectPrivilegedPorts: &models.GlobalHardenRejectPrivilegedPorts{
 				Quic: "enabled",
 			},
 		},
-		ThreadHardLimit:  misc.Int64P(100),
-		SslSecurityLevel: misc.Int64P(2),
-		HTTPErrCodes:     []*models.HTTPCodes{{Value: misc.StringP("100-150 -123 +599")}},
-		OcspUpdate:       &models.GlobalOcspUpdate{},
+		HTTPErrCodes: []*models.HTTPCodes{{Value: misc.StringP("100-150 -123 +599")}},
 	}
 }
