1341bf321f44f968a246cdae4712858a67f47462
Johann Behr <johann.behr@naval.cc>
2024-11-26 11:29:42 +0100

    MINOR: Add ClearCounters() and ClearCountersAll() to SingleRuntime

--- runtime/interface.go
+++ runtime/interface.go
@@ -129,6 +129,10 @@ type Info interface {
 type Manage interface {
 	// Reloads HAProxy's configuration file. Similar to SIGUSR2. Returns the startup logs.
 	Reload() (string, error)
+	// Clears max counters
+	ClearCounters() error
+	// Clears counters
+	ClearAllCounters() error
 }
 
 type Raw interface {
