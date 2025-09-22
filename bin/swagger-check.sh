513eca39e7880a26b70f9adfe24a26ec4fbc9335
Marko Juraga <mjuraga@haproxy.com>
2025-09-22 13:48:02 +0200

    BUILD/MINOR: go-swagger: upgrade go-swagger to v0.32.3
    Added GOTOOLCHAIN env to swagger-check.sh as go-swagger fails with 1.25.

--- bin/swagger-check.sh
+++ bin/swagger-check.sh
@@ -3,5 +3,5 @@ V=$(./swagger version | grep version | awk 'NF>1{print $NF}')
 
 case "$V" in
   *$SWAGGER_VERSION*) echo "swagger version $V is OK" ;;
-  *)          echo "Detected $V, Installing $SWAGGER_VERSION"; GOBIN=$(pwd) go install "github.com/go-swagger/go-swagger/cmd/swagger@$SWAGGER_VERSION" ;;
+  *)          echo "Detected $V, Installing $SWAGGER_VERSION"; GOTOOLCHAIN=go1.24.6 GOBIN=$(pwd) go install "github.com/go-swagger/go-swagger/cmd/swagger@$SWAGGER_VERSION" ;;
 esac
