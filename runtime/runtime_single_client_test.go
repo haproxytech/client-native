b572614165de1658131e8d42dda489a240609094
Olivier Duclos <oduclos@haproxy.com>
2026-03-27 10:56:15 +0100

    BUG/MEDIUM: runtime: reject special characters in filenames
    Avoid command injection when calling the runtime API by rejecting
    dangerous characters which should never be used in filenames.
    
    The rejected characters: \r\n<>*;$#&{}"
    
    Note that we cannot simply add quotes around arguments in runtime
    commands since HAProxy does not parse them, unlike in the configuration
    file.

---
+++ runtime/runtime_single_client_test.go
@@ -0,0 +1,46 @@
+// Copyright 2026 HAProxy Technologies
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package runtime_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/haproxytech/client-native/v6/runtime"
+)
+
+func TestSingleRuntime_Execute(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		command string
+		wantErr error
+	}{
+		{
+			name:    "two commands with semicolon",
+			command: "show ssl cert foo;dump ssl foo",
+			wantErr: runtime.ErrRuntimeInvalidChar,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var s runtime.SingleRuntime
+			gotErr := s.Execute(tt.command)
+			if !errors.Is(gotErr, tt.wantErr) {
+				t.Errorf("got %v, expected %v", gotErr, tt.wantErr)
+			}
+		})
+	}
+}
